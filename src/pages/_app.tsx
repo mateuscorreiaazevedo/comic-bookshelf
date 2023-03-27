63cb67764aa57b5ee5da52d331e67aaf7d11077d
Mateus <mateuscorreiaazevedo@gmail.com>
2023-03-27 15:22:42 -0300

    feat: add dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { ThemeProvider } from '@/modules/core'
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
-import { Layout } from '@/main/layout'
 
 export default function App ({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <ThemeProvider>
       <Component {...pageProps} />
       <ToastContainer />
-    </Layout>
+    </ThemeProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { useTheme } from '@/modules/core'
 import React from 'react'
 
 export default function Home () {
+  const { handleTheme } = useTheme()
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 onClick={handleTheme}>Hello World!</h1>
     </>
   )
 }
